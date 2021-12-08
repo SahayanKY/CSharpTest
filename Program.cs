e69878b7565b66a7c98a978e2fd92281e6ac5a11
SahayanKY <a1c4f0x324@gmail.com>
2021-12-09 05:46:56 +0900

    feat: ファイルを追加してそれを参照する方法を確認

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 
+
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ConsoleApp2.Test p = new ConsoleApp2.Test();
             Console.WriteLine("Hello World!");
         }
     }
---
+++ Test.cs
@@ -0,0 +1,7 @@
+
+namespace ConsoleApp2
+{
+    class Test
+    {
+    }
+}
